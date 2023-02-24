8655c49dcd6585d9b9b5419434a7fde33924d74b
shraddha0930 <shraddhaingole11@gmail.com>
2023-02-24 08:17:00 +0530

    Addded code to single player starting at 0

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,5 +2,9 @@ package assignments.SnakeAndLadder;
 
 public class SnakeAndLadder {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Snake and Ladder Program");    }
+        System.out.println("Welcome to the Snake and Ladder Program");
+        int START_POSITION = 0;
+        System.out.println("Starting postion is:" +START_POSITION);
+
+    }
 }
