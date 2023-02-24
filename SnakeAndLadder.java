3a06f7004a4aa12ec988bad765a14b2f81425d5e
shraddha0930 <shraddhaingole11@gmail.com>
2023-02-24 22:31:50 +0530

    Added code to play with 2 player and report winner

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,26 +4,47 @@ public class SnakeAndLadder {
     public static final int NO_PLAY = 0;
     public static final int LADDER = 1;
     public static final int SNAKE = 2;
+    static int START_POSITION = 0;
+    static int NEW_POSITION = 0;
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Snake and Ladder Program");
 
-        int START_POSITION = 0;
-        int NEW_POSITION = 0;
+        String p1 = "Player1";
+        String p2 = "Player2";
+
+
         int WIN_POSITION = 100;
-        int count=0;
+        int player1_position = 0;
+        int player2_position = 0;
+
 
         System.out.println("Starting position of Player is:" + START_POSITION);
+
+        int count1 = getCount(p1, player1_position, WIN_POSITION);
+        int count2 = getCount(p2, player2_position, WIN_POSITION);
+
+        //comparing and finding the winner
+        if (count1 > count2)
+            System.out.println(p2 + " is the winner ");
+        else if (count2 > count1)
+            System.out.println(p1 + "is the  winner");
+        else if (count1 == count2)
+            System.out.println("Match Draw");
+    }
+    public static int getCount(String name, int START_POSITION,int WIN_POSITION){
+        int count=0;
 
         while (START_POSITION < WIN_POSITION) {
 
             int DieRoll = (int) (Math.random() * 6) + 1;
             System.out.println("After rolling die number is:" + DieRoll);
+            count++;
 
             int CheckOption = (int) (Math.random() * 3);
             System.out.println("Checkoption is:" +CheckOption);
 
-            count++;
+
 
             switch (CheckOption) {
                 case NO_PLAY:
@@ -46,8 +67,10 @@ public class SnakeAndLadder {
             {
                 START_POSITION=NEW_POSITION;
             }
-            System.out.println("Now Player at " + START_POSITION + "th" + " Position");
+            System.out.println("Now"+ name +" at " + START_POSITION + "th" + " Position");
         }
-        System.out.println("Player won!! \nAfter Die Rolled "+ count +" Times");
+        System.out.println(name + "won!! \nAfter Die Rolled "+ count +" Times");
+
+        return count;
     }
 }
