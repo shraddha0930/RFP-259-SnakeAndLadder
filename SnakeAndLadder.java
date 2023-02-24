108c310eaf14f5abb395163b4404680e73f75411
shraddha0930 <114004352+shraddha0930@users.noreply.github.com>
2023-02-24 17:41:37 +0530

    Update SnakeAndLadder.java

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,7 +5,7 @@ public class SnakeAndLadder {
         System.out.println("Welcome to the Snake and Ladder Program");
 
         int START_POSITION = 0;
-        System.out.println("Starting postion is:" +START_POSITION);
+        System.out.println("Starting position is:" +START_POSITION);
 
         int DieRoll = (int) (Math.random()*6)+1;
         System.out.println("After rolling die number is:" +DieRoll);
