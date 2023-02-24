d0c56ccbb7cf89657498efd6440e7ea14bdc802b
shraddha0930 <shraddhaingole11@gmail.com>
2023-02-24 08:08:43 +0530

    Addded welcome message

---
+++ SnakeAndLadder.java
@@ -0,0 +1,6 @@
+package assignments.SnakeAndLadder;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Snake and Ladder Program");    }
+}
