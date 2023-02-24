f4bd930ac429bbe121388be16ddaa33934ecea66
shraddha0930 <shraddhaingole11@gmail.com>
2023-02-24 08:59:36 +0530

    Addded code to roll the die

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,8 +3,12 @@ package assignments.SnakeAndLadder;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to the Snake and Ladder Program");
+
         int START_POSITION = 0;
         System.out.println("Starting postion is:" +START_POSITION);
+
+        int DieRoll = (int) (Math.random()*6)+1;
+        System.out.println("After rolling die number is:" +DieRoll);
 
     }
 }
